e39359e22e618180f8e650ebda7e6dacf4982091
Angelika Fonteles <angelikatf@gmail.com>
2020-07-29 17:13:10 -0300

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez/Rei.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace xadrez_console.xadrez
+{
+    public class Rei
+    {
+        public Rei()
+        {
+        }
+    }
+}
---
+++ xadrez/Torre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace xadrez_console.xadrez
+{
+    public class Torre
+    {
+        public Torre()
+        {
+        }
+    }
+}
