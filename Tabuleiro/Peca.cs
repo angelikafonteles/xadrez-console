22ccfe3ef9a8335c16a2aa9008ec8ccd8f517f73
Angelika Fonteles <angelikatf@gmail.com>
2020-07-30 15:37:52 -0300

    Implementando as restricoes de movimento

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 namespace tabuleiro
 {
     abstract class Peca
@@ -19,6 +19,27 @@ namespace tabuleiro
         public void incrementarQteMovimentos()
         {
             qteMovimentos++;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for(int i = 0; i < tab.linhas; i++)
+            {
+                for(int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
         }
 
         public abstract bool[,] movimentosPossiveis();
