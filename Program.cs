edda092a42eca2ef9f65bbc6f1bd4828a1318ce8
Angelika Fonteles <angelikatf@gmail.com>
2020-07-29 21:07:38 -0300

    Corrigindo erro Tela

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(partida.tab);
 
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
